c3db445decb798cc910daa34e0873d3f5255f3da
Elizar Pepino <jupenz@gmail.com>
2013-11-25 03:03:52 +0800

    wrote first test

--- test/api.js
+++ test/api.js
@@ -1,1 +1,15 @@
 // Write some test
+var request = require('supertest'),
+    should = require('should'),
+    assert = require('assert');
+
+describe('You', function() {
+
+  // should make this pass
+  it('should return true if Your face is batig nawng', function() {
+    var You = {};
+    You.face = 'batig nawng';
+    You.face.should.equal('batig nawng');
+  });
+
+});

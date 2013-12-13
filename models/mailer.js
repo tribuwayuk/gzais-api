55cf6dd9ac02c3518ae67776e1aa82a6180869cc
Alexjander Bacalso <janderbacalso@gmail.com>
2013-12-13 16:31:23 +0800

    Fix API for reset password mailer
    - modify app.js
    - modify models/mailer.js
    - modify routes/employee.js

--- models/mailer.js
+++ models/mailer.js
@@ -1,6 +1,20 @@
 var nodemailer = require('nodemailer');
 
-exports.sendOne = function(sendTo, messageOptions) {
+exports.messageResetPassword = function ( data )
+{
+	// setup e-mail data      
+    var msgline      = "<br/>--------------------------------------------------------------------<br/>";
+    var msgGreetings = "Hi " + data['first_name'] + ' ' + data['last_name'] + ",<br/><br/>";
+    var msgSuccess   = "Your password has just been reset.";
+    var msgBody      = msgSuccess + "<br/>Your new login password: <b>"+ data['password'] + "</b><br/><br/><br/>";
+    var msgFooter    = "Thank you,<br/><br/>GZAIS Support Team<br/><br/>" + msgline + "<small>This is a system-generated email: Please do not reply.</small>";
+    
+    var msgTemplate  = msgGreetings + msgBody + msgFooter;
+
+    return msgTemplate;
+};
+
+exports.sendOne = function ( sendTo, messageOptions) {
 
     // create a defaultTransport using gmail and authentication
     var smtpTransport = nodemailer.createTransport("SMTP", {

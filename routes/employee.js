10fc21ac6d0a51224262f08c649211b7bec61294
Elizar Pepino <jupenz@gmail.com>
2013-12-16 15:06:03 +0800

    Fixed resetPassword issue when _id or email not exist

--- routes/employee.js
+++ routes/employee.js
@@ -129,25 +129,33 @@ exports.resetPassword = function( req, res ) {
     model.Employee.findOne( req.body, function( err, result ) {
 
         if ( err ) {
-            return res.end( JSON.stringify( err ) );
+           return res.end( JSON.stringify( err ) ); 
+        }
+
+        if ( !result ) {
+            var responseText          = {};
+
+            responseText.error        = 'not found';
+            responseText.error_messag = '_id or email does not exist';
+
+            return res.end( JSON.stringify( responseText ) );
         }
 
         result.password = newPassword.password;
-
         result.save( function ( err ) {
-			if ( err ) return res.end( JSON.stringify( err ) );
+            if ( err ) return res.end( JSON.stringify( err ) );
 
-			var msgTemplate    = mailer.messageResetPassword( data );
-			var msgSubject     = req.body._id ? "GZAIS | Request to Reset Password ( Reset by Admin )" : "GZAIS | Request to Reset Password ( Forgot Password )";
-			var messageOptions = {
-				subject              : msgSubject,
-				generateTextFromHTML : true,
-				html                 : msgTemplate
-			};
+            var msgTemplate    = mailer.messageResetPassword( data );
+            var msgSubject     = req.body._id ? "GZAIS | Request to Reset Password ( Reset by Admin )" : "GZAIS | Request to Reset Password ( Forgot Password )";
+            var messageOptions = {
+                subject              : msgSubject,
+                generateTextFromHTML : true,
+                html                 : msgTemplate
+            };
 
-			mailer.sendOne( result.email, messageOptions );
-			res.end( result );
-		});
+            mailer.sendOne( result.email, messageOptions );
+            res.end( result );
+        });
 
         return res.end( 'ok tteste' +  JSON.stringify(req.body));
     });

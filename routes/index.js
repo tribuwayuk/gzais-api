58854733a2dba93efb3349e36ecfaf82c76a7383
Elizar Pepino <jupenz@gmail.com>
2013-12-18 15:01:01 +0800

    Fixed duplicating assigned items

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var Employee    = mongoose.model( 'Employee' );
 
 exports.index = function( req, res ) {
 
-    var index     = { }
+    var index 	  = {};
     index.name    = 'GZAIS API';
     index.version = '0.0.1';
 
@@ -16,63 +16,69 @@ exports.index = function( req, res ) {
 
 exports.accessControl = function( req, res, next ) {
 
-	res.setHeader( 'Access-Control-Allow-Origin', '*' );
-	res.setHeader( 'Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS' );
-	res.setHeader( 'Access-Control-Allow-Headers', 'Content-Type' );
+    res.setHeader( 'Access-Control-Allow-Origin', '*' );
+    res.setHeader( 'Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS' );
+    res.setHeader( 'Access-Control-Allow-Headers', 'Content-Type' );
 
-	if ( req.method === 'OPTIONS' ) {
-	    return res.end( );
-	}
+    if ( req.method === 'OPTIONS' ) {
+        return res.end( );
+    }
 
-	res.setHeader( 'Content-Type', 'application/json' );
-	next( );
+    res.setHeader( 'Content-Type', 'application/json' );
+    next( );
 
 }
 
 exports.login = function( req, res ) {
 
-	Employee.findOne( { email: req.body.email, password: req.body.password }, function( err, employee ) {
+    Employee.findOne( {
+        email    : req.body.email,
+        password : req.body.password
+    }, function( err, employee ) {
 
+        if ( err ) {
+            return res.end( JSON.stringify( err ) );
+        }
+
+        // If log in fails, return a 403 status code and error message
+        if ( !employee ) {
+
+            var message 	= {};
+            message.error   = 'Access Denied';
+            message.message = 'Wrong email or password. Please try again!';
+
+            res.statusCode  = 403;
+            return res.end( JSON.stringify( message ) );
+
+        }
+
+        // Create access token for the successfully logged in user
+        var accessToken = new AccessToken( {
+            employee: employee._id
+        } );
+
+        accessToken.save( function( err ) {
             if ( err ) {
                 return res.end( JSON.stringify( err ) );
             }
 
-            // If log in fails, return a 403 status code and error message
-            if ( !employee ) {
+            // Find and populate access token's employee field
+            AccessToken.findOne( {
+                access_token: accessToken.access_token
+            } )
+                .populate( 'employee' )
+                .exec( function( err, newToken ) {
 
-            	var message = {};
+                    if ( err ) {
+                        return res.end( JSON.stringify( err ) );
+                    }
 
-            	message.error   = 'Access Denied';
-            	message.message = 'Wrong email or password. Please try again!';
+                    // return the complete access token object with the populated
+                    // employee field
+                    res.end( JSON.stringify( newToken ) );
 
-            	res.statusCode = 403;
-            	return res.end( JSON.stringify( message ) );
-
-            }
-
-            // Create access token for the successfully logged in user
-            var accessToken = new AccessToken( { employee: employee._id } );
-
-            accessToken.save( function( err ) {
-            	if ( err ) {
-            		return res.end( JSON.stringify( err ) );
-            	}
-
-            	// Find and populate access token's employee field
-            	AccessToken.findOne( { access_token : accessToken.access_token } )
-	            	.populate( 'employee' )
-	            	.exec( function( err, newToken ) {
-
-	            		if ( err ) {
-	            			return res.end( JSON.stringify( err ) );
-	            		}
-
-	            		// return the complete access token object with the populated
-	            		// employee field
-	            		res.end( JSON.stringify( newToken ) );
-
-	            	} );
-            } );
+                } );
+        } );
 
 
     } );
